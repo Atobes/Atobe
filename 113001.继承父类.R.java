8d8ec46bf1e39e604ed39c83fe5c9bd22736a66a
Atobes <74660162+Atobes@users.noreply.github.com>
2020-12-01 23:19:40 +0800

    Create 113001.继承父类.R.java

---
+++ 113001.继承父类.R.java
@@ -0,0 +1,9 @@
+package pac;
+public class R{
+
+    static void HuaBei() {
+            String boy_filePath = "img/boy.gif";
+            String girl_filePath = "img/girl.gif";
+
+            }
+    }
